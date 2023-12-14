652217cc8914ce9a269785f4cb3bcb17ad0fac46
kv45 <kshitijverma852@gmail.com>
2023-12-14 05:02:37 +0000

    setup and begin association testing

--- test/test_helper.js
+++ test/test_helper.js
@@ -13,7 +13,14 @@ before(done => {
 });
 
 beforeEach(done => {
-	mongoose.connection.collections.users.drop(() => done());
+	const { users, comments, blogposts } = mongoose.connection.collections;
+	users.drop(() => {
+		comments.drop(() => {
+			blogposts.drop(() => {
+				done();
+			});
+		});
+	});
 });
 
 after(done => {
